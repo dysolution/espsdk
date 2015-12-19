5408d32c87ad607a47a372764b8046f062cda591
Jordan Peterson <dysolution@gmail.com>
2015-12-19 17:09:19 -0600

    alpha sort

--- contribution.go
+++ contribution.go
@@ -5,28 +5,29 @@ import (
 )
 
 type Contribution struct {
-	CameraShotDate       string   `json:"camera_shot_date"`
-	CollectionCode       string   `json:"collection_code"`
-	ContentProviderName  string   `json:"content_provider_name"`
-	ContentProviderTitle string   `json:"content_provider_title"`
-	CountryOfShoot       string   `json:"country_of_shoot"`
-	CreditLine           string   `json:"credit_line"`
-	ExternalFileLocation string   `json:"external_file_location"`
-	FileName             string   `json:"file_name"`
-	FilePath             string   `json:"file_path"`
-	Headline             string   `json:"headline"`
-	IptcCategory         string   `json:"iptc_category"`
-	IptcSubjects         []string `json:"iptc_subjects"`
-	MimeType             string   `json:"mime_type"`
-	ParentSource         string   `json:"parent_source"`
-	RecordedDate         string   `json:"recorded_date"`
-	RiskCategory         string   `json:"risk_category"`
-	ShotSpeed            string   `json:"shot_speed"`
-	SiteDestination      []string `json:"site_destination"`
-	Source               string   `json:"source"`
-	SubmittedToReviewAt  string   `json:"submitted_to_review_at"`
-	UploadBucket         string   `json:"upload_bucket"`
-	UploadId             string   `json:"upload_id"`
+	CameraShotDate       string   `json:"camera_shot_date,omitempty"`
+	CollectionCode       string   `json:"collection_code,omitempty"`
+	ContentProviderName  string   `json:"content_provider_name,omitempty"`
+	ContentProviderTitle string   `json:"content_provider_title,omitempty"`
+	CountryOfShoot       string   `json:"country_of_shoot,omitempty"`
+	CreditLine           string   `json:"credit_line,omitempty"`
+	ExternalFileLocation string   `json:"external_file_location,omitempty"`
+	FileName             string   `json:"file_name,omitempty"`
+	FilePath             string   `json:"file_path,omitempty"`
+	Headline             string   `json:"headline,omitempty"`
+	Id                   string   `json:"id,omitempty"`
+	IptcCategory         string   `json:"iptc_category,omitempty"`
+	IptcSubjects         []string `json:"iptc_subjects,omitempty"`
+	MimeType             string   `json:"mime_type,omitempty"`
+	ParentSource         string   `json:"parent_source,omitempty"`
+	RecordedDate         string   `json:"recorded_date,omitempty"`
+	RiskCategory         string   `json:"risk_category,omitempty"`
+	ShotSpeed            string   `json:"shot_speed,omitempty"`
+	SiteDestination      []string `json:"site_destination,omitempty"`
+	Source               string   `json:"source,omitempty"`
+	SubmittedToReviewAt  string   `json:"submitted_to_review_at,omitempty"`
+	UploadBucket         string   `json:"upload_bucket,omitempty"`
+	UploadId             string   `json:"upload_id,omitempty"`
 }
 
 func (c Contribution) Marshal() ([]byte, error) {
