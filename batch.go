6809c3b2cfa82a1a159ce32e6f00969dfb7f4e71
Jordan Peterson <dysolution@gmail.com>
2015-12-19 16:52:03 -0600

    account for all Submission Batch fields

--- batch.go
+++ batch.go
@@ -1,17 +1,39 @@
 package espsdk
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 type SubmissionBatchType string
 
 type SubmissionBatch struct {
-	SubmissionName        string `json:"submission_name,omitempty"`
-	SubmissionType        string `json:"submission_type,omitempty"`
-	Note                  string `json:"note,omitempty"`
-	SaveExtractedMetadata bool   `json:"save_extracted_metadata,omitempty"`
-	AssignmentId          string `json:"assignment_id,omitempty"`
-	BriefId               string `json:"brief_id,omitempty"`
-	EventId               string `json:"event_id,omitempty"`
+	AssignmentId                     string     `json:"assignment_id,omitempty"`
+	BatchTags                        []string   `json:"batch_tags,omitempty"`
+	BriefId                          string     `json:"brief_id,omitempty"`
+	ContributionsAwaitingReviewCount int        `json:"contributions_awaiting_review_count"`
+	ContributionsCount               int        `json:"contributions_count"`
+	CreatedAt                        time.Time  `json:"created_at"`
+	CreatedBy                        string     `json:"created_by"`
+	CreatorIstockUsername            string     `json:"creator_istock_username"`
+	EventId                          string     `json:"event_id,omitempty"`
+	Id                               int        `json:"id"`
+	IsGetty                          bool       `json:"is_getty"`
+	IsIstock                         bool       `json:"is_istock"`
+	IstockExclusive                  bool       `json:"istock_exclusive"`
+	LastContributionSubmittedAt      *time.Time `json:"last_contribution_submitted_at,omitempty"`
+	LastSubmittedAt                  *time.Time `json:"last_submitted_at,omitempty"`
+	Note                             string     `json:"note,omitempty"`
+	ProfileId                        int        `json:"profile_id"`
+	ReviewedContributionsCount       int        `json:"reviewed_contributions_count"`
+	RevisableContributionsCount      int        `json:"revisable_contributions_count"`
+	SaveExtractedMetadata            bool       `json:"save_extracted_metadata,omitempty"`
+	Status                           string     `json:"status,omitempty"`
+	SubmissionName                   string     `json:"submission_name,omitempty"`
+	SubmissionType                   string     `json:"submission_type,omitempty"`
+	SubmittedContributionsCount      int        `json:"submitted_contributions_count,omitempty"`
+	UpdatedAt                        time.Time  `json:"updated_at"`
+	UserId                           string     `json:"user_id"`
 }
 
 func (s SubmissionBatch) Marshal() ([]byte, error) { return json.MarshalIndent(s, "", "  ") }
