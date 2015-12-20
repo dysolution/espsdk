d1b5a39d7a0723d62cefcd15a50a95a7e8ecb21b
Jordan Peterson <dysolution@gmail.com>
2015-12-19 21:20:28 -0600

    omit all empty fields in serializing a SubmissionBatch so that its type can be used as a minimal set of changes during Update

--- batch.go
+++ batch.go
@@ -12,29 +12,29 @@ type SubmissionBatch struct {
 	AssignmentID                     string     `json:"assignment_id,omitempty"`
 	BatchTags                        []string   `json:"batch_tags,omitempty"`
 	BriefID                          string     `json:"brief_id,omitempty"`
-	ContributionsAwaitingReviewCount int        `json:"contributions_awaiting_review_count"`
-	ContributionsCount               int        `json:"contributions_count"`
-	CreatedAt                        time.Time  `json:"created_at"`
-	CreatedBy                        string     `json:"created_by"`
-	CreatorIstockUsername            string     `json:"creator_istock_username"`
+	ContributionsAwaitingReviewCount int        `json:"contributions_awaiting_review_count,omitempty"`
+	ContributionsCount               int        `json:"contributions_count,omitempty"`
+	CreatedAt                        *time.Time `json:"created_at,omitempty"`
+	CreatedBy                        string     `json:"created_by,omitempty"`
+	CreatorIstockUsername            string     `json:"creator_istock_username,omitempty"`
 	EventID                          string     `json:"event_id,omitempty"`
-	ID                               int        `json:"id"`
-	IsGetty                          bool       `json:"is_getty"`
-	IsIstock                         bool       `json:"is_istock"`
-	IstockExclusive                  bool       `json:"istock_exclusive"`
+	ID                               int        `json:"id,omitempty"`
+	IsGetty                          bool       `json:"is_getty,omitempty"`
+	IsIstock                         bool       `json:"is_istock,omitempty"`
+	IstockExclusive                  bool       `json:"istock_exclusive,omitempty"`
 	LastContributionSubmittedAt      *time.Time `json:"last_contribution_submitted_at,omitempty"`
 	LastSubmittedAt                  *time.Time `json:"last_submitted_at,omitempty"`
 	Note                             string     `json:"note,omitempty"`
-	ProfileID                        int        `json:"profile_id"`
-	ReviewedContributionsCount       int        `json:"reviewed_contributions_count"`
-	RevisableContributionsCount      int        `json:"revisable_contributions_count"`
+	ProfileID                        int        `json:"profile_id,omitempty"`
+	ReviewedContributionsCount       int        `json:"reviewed_contributions_count,omitempty"`
+	RevisableContributionsCount      int        `json:"revisable_contributions_count,omitempty"`
 	SaveExtractedMetadata            bool       `json:"save_extracted_metadata,omitempty"`
 	Status                           string     `json:"status,omitempty"`
 	SubmissionName                   string     `json:"submission_name,omitempty"`
 	SubmissionType                   string     `json:"submission_type,omitempty"`
 	SubmittedContributionsCount      int        `json:"submitted_contributions_count,omitempty"`
-	UpdatedAt                        time.Time  `json:"updated_at"`
-	UserID                           string     `json:"user_id"`
+	UpdatedAt                        *time.Time `json:"updated_at,omitempty"`
+	UserID                           string     `json:"user_id,omitempty"`
 }
 
 func (s SubmissionBatch) Marshal() ([]byte, error) { return json.MarshalIndent(s, "", "  ") }
@@ -60,16 +60,11 @@ func (b SubmissionBatch) PrettyPrint() string {
 }
 
 type SubmissionBatchUpdate struct {
-	SubmissionBatch SubmissionBatchChanges `json:"submission_batch"`
+	SubmissionBatch SubmissionBatch `json:"submission_batch"`
 }
 
 func (s SubmissionBatchUpdate) Marshal() ([]byte, error) {
 	return json.MarshalIndent(s, "", "  ")
-}
-
-type SubmissionBatchChanges struct {
-	SubmissionName string `json:"submission_name,omitempty"`
-	Note           string `json:"note,omitempty"`
 }
 
 var batchTypeIsValid = map[string]bool{
