28777bdd255ad13ed71f3663984dea624e006bfe
Jordan Peterson <dysolution@gmail.com>
2015-12-20 02:11:34 -0600

    move endpoint paths to SDK

---
+++ endpoints.go
@@ -0,0 +1,13 @@
+package espsdk
+
+// These constants represent the relative paths for various ESP API endpoints.
+const (
+	Batches            string = "/submission/v1/submission_batches"
+	ControlledValues   string = "/submission/v1/controlled_values/index"
+	Keywords           string = "/submission/v1/keywords/getty"
+	Personalities      string = "/submission/v1/personalities"
+	TranscoderMappings string = "/submission/v1/video_transcoder_mapping_values"
+	Compositions       string = "/submission/v1/people_metadata/compositions"
+	Expressions        string = "/submission/v1/people_metadata/expressions"
+	NumberOfPeople     string = "/submission/v1/people_metadata/number_of_people"
+)
