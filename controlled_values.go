1aae64751db3d43f33659fe17c3eaf2252e95018
Jordan Peterson <dysolution@gmail.com>
2015-12-20 20:53:14 -0600

    update docs

--- controlled_values.go
+++ controlled_values.go
@@ -1,9 +1,26 @@
 package espsdk
 
-func GetKeywords(client *Client) []byte           { return client.get(Keywords) }
-func GetPersonalities(client *Client) []byte      { return client.get(Personalities) }
-func GetControlledValues(client *Client) []byte   { return client.get(ControlledValues) }
+// GetKeywords requests suggestions from the Getty controlled vocabulary
+// for the keywords provided.
+func GetKeywords(client *Client) []byte { return client.get(Keywords) }
+
+// GetPersonalities requests suggestions from the Getty controlled vocabulary
+// for the famous personalities provided.
+func GetPersonalities(client *Client) []byte { return client.get(Personalities) }
+
+// GetControlledValues returns complete lists of values and descriptions for
+// fields with controlled vocabularies, grouped by submission type.
+func GetControlledValues(client *Client) []byte { return client.get(ControlledValues) }
+
+// GetTranscoderMappings lists acceptable transcoder mapping values
+// for Getty and iStock video.
 func GetTranscoderMappings(client *Client) []byte { return client.get(TranscoderMappings) }
-func GetCompositions(client *Client) []byte       { return client.get(Compositions) }
-func GetExpressions(client *Client) []byte        { return client.get(Expressions) }
-func GetNumberOfPeople(client *Client) []byte     { return client.get(NumberOfPeople) }
+
+// GetCompositions lists all possible composition values.
+func GetCompositions(client *Client) []byte { return client.get(Compositions) }
+
+// GetExpressions lists all possible facial expression values.
+func GetExpressions(client *Client) []byte { return client.get(Expressions) }
+
+// GetNumberOfPeople lists all possible values for Number of People.
+func GetNumberOfPeople(client *Client) []byte { return client.get(NumberOfPeople) }
