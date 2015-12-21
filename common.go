2d8f51fd5d610398f5820b52f1770f7eeef44f07
Jordan Peterson <dysolution@gmail.com>
2015-12-20 23:01:33 -0600

    update docs

--- common.go
+++ common.go
@@ -6,6 +6,57 @@ You will need a username and password that allows you
 to log in to https://sandbox.espaws.com/ as well as
 an API Key and and API Secret, both of which are accessible
 at https://developer.gettyimages.com/apps/mykeys/.
+
+	// Configure the SDK's client with your credentials.
+	client := espsdk.Client{
+		Credentials: espsdk.Credentials{
+			APIKey:      "esp_api_key",
+			APISecret:   "esp_api_secret",
+			ESPUsername: "esp_username",
+			ESPPassword: "esp_password",
+		},
+		UploadBucket: "oregon",
+	}
+
+	// Get a token based on those credentials.
+	token := client.GetToken()
+
+	data := espsdk.Batch{
+		SubmissionName: "My Photos",
+		SubmissionType: "getty_creative_still",
+	}
+	batch := espsdk.Batch{}.Create(&client, data)
+
+	// Get a list of batches, which should include the one you just created.
+	batches := espsdk.Batch{}.Index(&client)
+
+You can proceed from there to add contributions:
+    batchID := 81421  // iterate Batch{}.Index() to get these
+    data := espsdk.Contribution{
+        Headline: "My Photo Title",
+        FileName: "IMG_9235.JPG",
+    }
+    espsdk.Contribution{}.Create(&client, batchID, data)
+
+You can also add Releases to a batch:
+    batchID := 81421
+    data := espsdk.Release{
+    ReleaseType: "Property",
+        FileName: "IMG_1735.JPG",
+        MimeType: "image/jpeg",
+    }
+    espsdk.Release{}.Create(&client, batchID, data)
+
+Contributions, Releases, and Batches can be deleted as well:
+    batchID := 81421
+    releaseID := 172421  // iterate Release{}.Index() to get these
+    espsdk.Release{ID: releaseID}.Delete(&client, batchID)
+
+Each of the three main types has a consistent CRUD interface. Other API
+endpoints are expressed either as simple GETs or endpoints that perform
+auto-suggest against provided terms in order to match them to Getty's
+controlled vocabularies, such as suggesting "Bob Newhart" for "Bob".
+
 */
 package espsdk
 
@@ -15,6 +66,9 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 )
+
+func foo() {
+}
 
 // A Token is a string representation of an OAuth2 token. It grants a user
 // access to the ESP API for a limited time.
