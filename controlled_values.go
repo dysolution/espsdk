c9a258df99e7bb3ccd734736552eb431702d6403
Jordan Peterson <dysolution@gmail.com>
2015-12-20 21:38:14 -0600

    enable deserialziation of number_of_people responses

--- controlled_values.go
+++ controlled_values.go
@@ -1,5 +1,10 @@
 package espsdk
 
+import (
+	"encoding/json"
+	"log"
+)
+
 // GetKeywords requests suggestions from the Getty controlled vocabulary
 // for the keywords provided.
 func GetKeywords(client *Client) []byte { return client.get(Keywords) }
@@ -22,5 +27,33 @@ func GetCompositions(client *Client) []byte { return client.get(Compositions) }
 // GetExpressions lists all possible facial expression values.
 func GetExpressions(client *Client) []byte { return client.get(Expressions) }
 
+type PeopleMetadata struct {
+	Term     string `json:"term,omitempty"`
+	TermID   int    `json:"term_id,omitempty"`
+	ImageURI string `json:"image_uri,omitempty"`
+	HelpText string `json:"help_text,omitempty"`
+}
+
+type PeopleMetadataList []PeopleMetadata
+
+// Marshal serializes PeopleMetadata into a byte slice of indented JSON.
+func (m PeopleMetadataList) Marshal() ([]byte, error) { return indentedJSON(m) }
+
+// Unmarshal attempts to deserialize the provided JSON payload into a
+// representation of people metadata.
+func (m PeopleMetadataList) Unmarshal(payload []byte) PeopleMetadataList {
+	var items PeopleMetadataList
+	if err := json.Unmarshal(payload, &items); err != nil {
+		log.Fatal(err)
+	}
+	return items
+}
+
 // GetNumberOfPeople lists all possible values for Number of People.
-func GetNumberOfPeople(client *Client) []byte { return client.get(NumberOfPeople) }
+func (m PeopleMetadataList) GetNumberOfPeople(client *Client) PeopleMetadataList {
+	return m.Unmarshal(client.get(NumberOfPeople))
+}
+
+// PrettyPrint returns a human-readable serialized JSON representation of
+// the provided object.
+func (m PeopleMetadataList) PrettyPrint() string { return prettyPrint(m) }
