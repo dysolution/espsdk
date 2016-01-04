c5ffdc998b752781967efb4ab45150740f1376f6
Jordan Peterson <dysolution@gmail.com>
2016-01-03 21:43:44 -0600

    update docs to reflect API changes

--- doc.go
+++ doc.go
@@ -7,7 +7,8 @@ to log in to https://sandbox.espaws.com/ as well as
 an API Key and and API Secret, both of which are accessible
 at https://developer.gettyimages.com/apps/mykeys/.
 
-	// Configure the SDK's client with your credentials.
+First, configure the SDK's client with your credentials:
+
 	client := espsdk.GetClient(
 		"esp_api_key",
 		"esp_api_secret",
@@ -16,8 +17,13 @@ at https://developer.gettyimages.com/apps/mykeys/.
 		"oregon",
 		)
 
-	// Get a token based on those credentials.
+The client creates and sends a token along with each request. If you'd like
+to save and cache it, you can call GetToken directly:
+
 	token := client.GetToken()
+
+Media assets, such as photos and videos (represented as Contributions) must be
+uploaded into Submission Batches. You can use the client to create a Submission Batch:
 
 	data := espsdk.Batch{
 		SubmissionName: "My Photos",
@@ -25,7 +31,8 @@ at https://developer.gettyimages.com/apps/mykeys/.
 	}
 	batch := client.Create(data.Path(), data)
 
-	// Get a list of batches, which should include the one you just created.
+The list (index) of your Batches now includes the Batch you just created:
+
 	batches := client.Index(espsdk.Batches)
 
 You can proceed from there to add contributions:
@@ -34,7 +41,13 @@ You can proceed from there to add contributions:
         Headline: "My Photo Title",
         FileName: "IMG_9235.JPG",
     }
-		client.Create(data.Path(), data)
+    client.Create(data.Path(), data)
+
+The list (index) of Contributions for the Batch now includes the one you
+just created:
+
+    batchID := 81421
+    contributions := espsdk.Contribution{}.Index(&client, batchID)
 
 You can also add Releases to a batch:
     batchID := 81421
@@ -43,14 +56,17 @@ You can also add Releases to a batch:
         FileName: "IMG_1735.JPG",
         MimeType: "image/jpeg",
     }
-    espsdk.Release{}.Create(&client, batchID, data)
-		client.Create(data.Path(), data)
+    client.Create(data.Path(), data)
+
+The list (index) of Releases for the Batch now includes the one you
+just created:
+
+    batchID := 81421
+    releases := espsdk.Release{}.Index(&client, batchID)
 
 Contributions, Releases, and Batches can be deleted as well:
-    batchID := 81421
-    releaseID := 172421  // iterate Release{}.Index() to get these
-		release := espsdk.Release{ID: 172421, SubmissionBatchID: 81421}
-		client.Delete(release.Path())
+    release := espsdk.Release{ID: 172421, SubmissionBatchID: 81421}
+    client.Delete(release.Path())
 
 Each of the three main types has a consistent CRUD interface. Other API
 endpoints are expressed either as simple GETs or endpoints that perform
