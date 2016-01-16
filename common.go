7009b2ca7c23d9e97b5af5866d2d41eb3ed4f156
Jordan Peterson <dysolution@gmail.com>
2016-01-15 21:01:50 -0600

    move all result-related functionality to separate file

--- common.go
+++ common.go
@@ -2,9 +2,6 @@ package espsdk
 
 import (
 	"encoding/json"
-	"time"
-
-	log "github.com/Sirupsen/logrus"
 )
 
 // A Token is a string representation of an OAuth2 token. It grants a user
@@ -14,55 +11,6 @@ type Token string
 // A Serializable object can be serialized to a byte stream such as JSON.
 type serializable interface {
 	Marshal() ([]byte, error)
-}
-
-// A FulfilledRequest provides an overview of a completed API request and
-// its result, including timing and HTTP status codes.
-type Result struct {
-	*request
-	*VerboseResult
-}
-
-// Marshal serializes a FulfilledRequest into a byte stream.
-func (r *Result) Marshal() ([]byte, error) {
-	return json.Marshal(r)
-}
-
-// MarshalIndent serializes a FulfilledRequest into indented JSON.
-func (r *Result) MarshalIndent() ([]byte, error) {
-	return json.MarshalIndent(r, "", "    ")
-}
-
-// Stats returns fields that logrus can parse.
-func (r *Result) Stats() log.Fields {
-	return log.Fields{
-		"method":        r.Verb,
-		"path":          r.Path,
-		"response_time": r.Duration * time.Millisecond,
-		"status":        r.Response.Status,
-		"status_code":   r.Response.StatusCode,
-	}
-}
-
-func (r *Result) Log() *log.Entry {
-	return log.WithFields(r.Stats())
-}
-
-func (r *Result) GetStatusCode() int {
-	return r.VerboseResult.GetStatusCode()
-}
-
-// Private
-
-// A Response contains the HTTP status code and text that represent the API's
-// response to a request.
-type response struct {
-	StatusCode int    `json:"status_code"`
-	Status     string `json:"status"`
-}
-
-func (r *response) GetStatusCode() int {
-	return r.StatusCode
 }
 
 func indentedJSON(obj interface{}) ([]byte, error) {
--- result.go
+++ result.go
@@ -1,6 +1,7 @@
 package espsdk
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -8,8 +9,60 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-// A Result contains information relative to a completed request, including
-// the time elapsed to fulfill the request and any errors.
+// A Result provides an overview of a completed API request and
+// its result, including timing and HTTP status codes.
+type Result struct {
+	*request
+	*VerboseResult
+}
+
+// Marshal serializes a FulfilledRequest into a byte stream.
+func (r *Result) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+// MarshalIndent serializes a FulfilledRequest into indented JSON.
+func (r *Result) MarshalIndent() ([]byte, error) {
+	return json.MarshalIndent(r, "", "    ")
+}
+
+// Stats returns fields that logrus can parse.
+func (r *Result) Stats() log.Fields {
+	return log.Fields{
+		"method":        r.Verb,
+		"path":          r.Path,
+		"response_time": r.Duration * time.Millisecond,
+		"status":        r.Response.Status,
+		"status_code":   r.Response.StatusCode,
+	}
+}
+
+func (r *Result) Log() *log.Entry {
+	return log.WithFields(log.Fields{
+		"response_time": r.Duration * time.Millisecond,
+		"status_code":   r.Response.StatusCode,
+	})
+}
+
+func (r *Result) GetStatusCode() int {
+	return r.VerboseResult.GetStatusCode()
+}
+
+// Private
+
+// A Response contains the HTTP status code and text that represent the API's
+// response to a request.
+type response struct {
+	StatusCode int    `json:"status_code"`
+	Status     string `json:"status"`
+}
+
+func (r *response) GetStatusCode() int {
+	return r.StatusCode
+}
+
+// A VerboseResult contains information relative to a completed request,
+// including the time elapsed to fulfill the request and any errors.
 type VerboseResult struct {
 	Response *response     `json:"response"`
 	Payload  []byte        `json:"-"`
