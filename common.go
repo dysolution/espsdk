dfb2cd80a2839dd6a3a4152f5e77e337c9f160a2
Jordan Peterson <dysolution@gmail.com>
2016-01-16 17:55:44 -0600

    docs: obey the linter

--- common.go
+++ common.go
@@ -8,6 +8,9 @@ import (
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
+// Log is an instance of logrus.(*Logger) that allows easy access to
+// nicely-formatted log output that includes properties of completed HTTP
+// requests such as the response time and status code.
 var Log = logrus.New()
 
 func init() {
