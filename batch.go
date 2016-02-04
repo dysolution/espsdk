f54b6b869191e0a7e6ed039debe12fe34c03ce26
Jordan Peterson <dysolution@gmail.com>
2016-02-03 23:21:08 -0600

    feat: improve output of Batch Index

--- batch.go
+++ batch.go
@@ -122,12 +122,12 @@ var batchTypeIsValid = map[string]bool{
 // A BatchList matches the structure of the JSON payload returned
 // by the GET (all) Batches API endpoint.
 type BatchList struct {
-	Items             []Batch `json:"items,omitempty"`
-	batchListMetadata `json:"meta,omitempty"`
+	Items             []Batch `json:"items"`
+	batchListMetadata `json:"meta"`
 }
 
 type batchListMetadata struct {
-	TotalItems int `json:"total_items,omitempty"`
+	TotalItems int `json:"total_items"`
 }
 
 // Unmarshal attempts to deserialize the provided JSON payload
